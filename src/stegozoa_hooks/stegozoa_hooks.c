0a311653e3918aa4a5a7cfd12c9682df63536122
Gabriel Figueira <gabriel.figueira@tecnico.ulisboa.pt>
2021-03-09 18:51:00 +0000

    Debugging with printf, why not

--- src/stegozoa_hooks/stegozoa_hooks.c
+++ src/stegozoa_hooks/stegozoa_hooks.c
@@ -2,15 +2,17 @@
 #include <stdio.h>
 
 char msg[] = "AAAAAAAAAAAAAAAAAAAA";
+static int stop = 0;
 
 void writeQdctLsb(short *qcoeff) {
 
     int n_bits = sizeof(msg) * 8;
-
+    if(stop < 2) {
     //future idea: loop unroll
     for(int i = 0; i < 400 && i < n_bits; i++) {
         short bit = (msg[i / 8] >> (i % 8)) & 1;
         qcoeff[i] = qcoeff[i] & (0xFFFE | bit);
+    }
     }
     
 }
@@ -19,7 +21,7 @@ void printQdct(short *qcoeff) {
 
     FILE *fp;
 
-    fp = fopen("/home/vagrant/qcoeff.txt", "a");
+/*    fp = fopen("/home/vagrant/qcoeff.txt", "a");
 
     if(!fp) {
         printf("Stegozoa: Couldnt open file");
@@ -31,7 +33,15 @@ void printQdct(short *qcoeff) {
         fprintf(fp, "%d", qcoeff[i]);
     }
 
-    fclose(fp);
+    fclose(fp);*/
 
+    if(stop < 2) {
+        stop++;
+    for(int i = 0; i < 400; i++) {
+        if (i % 16 == 0)
+            fprintf(fp, "\n");
+        fprintf(fp, "%d", qcoeff[i]);
+    }
+    }
 }
 
