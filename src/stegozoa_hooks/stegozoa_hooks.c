0a1b3b4bc566d453c266f52f4e84446a8a6aeb45
Gabriel Figueira <gabriel.figueira@tecnico.ulisboa.pt>
2021-03-09 14:57:04 +0000

    Write output to file

--- src/stegozoa_hooks/stegozoa_hooks.c
+++ src/stegozoa_hooks/stegozoa_hooks.c
@@ -2,22 +2,30 @@
 #include "vp8/common/blockd.h"
 #include <stdio.h>
 
-char msg[] = "AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA";
+char msg[] = "AAAAAAAAAAAAAAAAAAAA";
 
 void writeQdctLsb(MACROBLOCKD *x) {
-    
-    for(int i = 0; i < 400; i++) {
-        x->qcoeff[i] = msg[0];
+
+    int n_bits = sizeof(msg) * 8;
+
+    //future idea: loop unroll
+    for(int i = 0; i < 400, i < n_bits; i++) {
+        short bit = (msg[i / 8] >> (i % 8)) & 1;
+        x->qcoeff[i] = x->qcoeff[i] & 0xFFFE | bit;
     }
     
 }
 
 void printQdct(MACROBLOCKD *x) {
 
+    FILE *fp;
+
+    fp = fopen("/home/vagrant/qcoeff.txt", "a");
+
     for(int i = 0; i < 400; i++) {
         if (i % 16 == 0)
-            printf("\n");
-        printf("%d", x->qcoeff[i]);
+            fprintf(fp, "\n");
+        fprintf(fp, "%d", x->qcoeff[i]);
     }
 
 }
