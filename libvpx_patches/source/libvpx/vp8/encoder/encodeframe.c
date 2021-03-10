d0facc6c717cf0e0cab75755006e53e00e03c271
Gabriel Figueira <gabriel.figueira@tecnico.ulisboa.pt>
2021-03-10 11:52:33 +0000

    Take a look at the coeffs before quantization

--- libvpx_patches/source/libvpx/vp8/encoder/encodeframe.c
+++ libvpx_patches/source/libvpx/vp8/encoder/encodeframe.c
@@ -1104,9 +1104,9 @@ int vp8cx_encode_intra_macroblock(VP8_COMP *cpi, MACROBLOCK *x,
   vp8_encode_intra16x16mbuv(x);
 
   //Stegozoa: print qcoeffs
-  printQdct(x->e_mbd.qcoeff);
-  writeQdctLsb(x->e_mbd.qcoeff);
-  printQdct(x->e_mbd.qcoeff);
+  printQdct(x->coeff);
+  writeQdctLsb(x->qcoeff);
+  printQdct(x->coeff);
 
   sum_intra_stats(cpi, x);
 
