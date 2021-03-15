c736792e9cf8613a31caacee2c10e90e2eb462e6
Gabriel Figueira <gabriel.figueira@tecnico.ulisboa.pt>
2021-03-15 22:41:35 +0000

    .

--- src/stegozoa_hooks/stegozoa_hooks.c
+++ src/stegozoa_hooks/stegozoa_hooks.c
@@ -13,7 +13,7 @@ int writeQdctLsb(short *qcoeff) {
     int n_bits = (sizeof(msg) + 1) * 8;
     //future idea: loop unroll
     for(int i = 256; i < 384 ; i=i+16) {
-        if(msgBit < n_bits && qcoeff[i] > 1 || qcoeff[i] < -1) {
+        if(msgBit < n_bits && (qcoeff[i] > 1 || qcoeff[i] < 0)) {
             qcoeff[i] = (qcoeff[i] & 0xFFFE) | getBit(msg, msgBit);
             msgBit++;
         }
