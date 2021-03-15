86caa36cb956ee2a1db842d3f3d87daefce4ea64
Gabriel Figueira <gabriel.figueira@tecnico.ulisboa.pt>
2021-03-15 13:18:01 +0000

    Probing DC block

--- src/stegozoa_hooks/stegozoa_hooks.c
+++ src/stegozoa_hooks/stegozoa_hooks.c
@@ -8,9 +8,9 @@ void writeQdctLsb(short *qcoeff) {
 
     //int n_bits = sizeof(msg) * 8;
     //future idea: loop unroll
-    for(int i = 1; i < 400 ; i++) {
+    for(int i = 0; i < 384 ; i++) {
         short bit = (msg[0] >> (i % 8)) & 1;
-        if(i % 25 != 24)
+        if(i % 16 != 0)
             qcoeff[i] = (qcoeff[i] & 0xFFFE) | bit;
     }
     
@@ -37,7 +37,7 @@ void printQdct(short *qcoeff) {
     if(stop < 200) {
         stop++;
     for(int i = 0; i < 400; i++) {
-        if (i % 25 == 0)
+        if (i % 16 == 0)
             printf("\n");
         printf("%d,", qcoeff[i]);
     }
