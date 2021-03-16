43a6adbbab151aa4a0737211fdd5e9f8d814bc2a
Gabriel Figueira <gabriel.figueira@tecnico.ulisboa.pt>
2021-03-16 17:36:40 +0000

    ..

--- src/stegozoa_hooks/stegozoa_hooks.c
+++ src/stegozoa_hooks/stegozoa_hooks.c
@@ -58,16 +58,16 @@ void readQdctLsb(short *qcoeff) {
     for(int i = 0; i < 384 ; i=i+16) {
         if(qcoeff[i] > 1 || qcoeff[i] < 0) {
             if(stop++ < 1) {
-                printf("%d\n", getBit(msg, 0);
-                printf("%d\n", getBit(msg, 1);
-                printf("%d\n", getBit(msg, 2);
-                printf("%d\n", getBit(msg, 3);
-                printf("%d\n", getBit(msg, 4);
-                printf("%d\n", getBit(msg, 5);
-                printf("%d\n", getBit(msg, 6);
-                printf("%d\n", getBit(msg, 7);
-                printf("%d\n", getBit(msg, 8);
-                printf("%d\n", getBit(msg, 9);
+                printf("%d\n", getBit(msg, 0));
+                printf("%d\n", getBit(msg, 1));
+                printf("%d\n", getBit(msg, 2));
+                printf("%d\n", getBit(msg, 3));
+                printf("%d\n", getBit(msg, 4));
+                printf("%d\n", getBit(msg, 5));
+                printf("%d\n", getBit(msg, 6));
+                printf("%d\n", getBit(msg, 7));
+                printf("%d\n", getBit(msg, 8));
+                printf("%d\n", getBit(msg, 9));
             }
             setBit(msgReceived, msgBitDec, getLsb(qcoeff[i]));
             msgBitDec++;
