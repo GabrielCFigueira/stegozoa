428c20071c1b43c87255941a11616699a4e418d7
Gabriel Figueira <gabriel.figueira@tecnico.ulisboa.pt>
2021-03-17 12:01:53 +0000

    Some prints

--- src/stegozoa_hooks/stegozoa_hooks.c
+++ src/stegozoa_hooks/stegozoa_hooks.c
@@ -113,7 +113,9 @@ void readQdctLsb(short *qcoeff) {
             }
         }
         else if (msgBitDec == 8 && msgReceived[0] != '!') {
+            printf("msgReceived: %x\n", msgReceived[0]);
             msgReceived[0] >> 1;
+            printf("msgReceived: %x\n", msgReceived[0]);
             msgBitDec--;
         
         }
