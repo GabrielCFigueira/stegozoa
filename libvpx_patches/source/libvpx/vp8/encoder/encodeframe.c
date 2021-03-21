d454d40d700fb2c0b6ccc7e18e57496fa4ef61e5
Gabriel Figueira <gabriel.figueira@tecnico.ulisboa.pt>
2021-03-21 18:56:27 +0000

    60

--- libvpx_patches/source/libvpx/vp8/encoder/encodeframe.c
+++ libvpx_patches/source/libvpx/vp8/encoder/encodeframe.c
@@ -1164,7 +1164,7 @@ int vp8cx_encode_intra_macroblock(VP8_COMP *cpi, MACROBLOCK *x,
   vp8_tokenize_mb(cpi, x, t);
 
   if (mb_row == 5 && mb_col == 5) {
-    printTokens(tp+4);
+    printTokens(tp+60);
   }
   if (xd->mode_info_context->mbmi.mode != B_PRED) vp8_inverse_transform_mby(xd);
 
