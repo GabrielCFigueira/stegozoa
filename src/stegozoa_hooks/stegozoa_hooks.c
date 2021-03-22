9f64d6e5a20ad00e0223a8d0f90cdd52c72ac310
Gabriel Figueira <gabriel.figueira@tecnico.ulisboa.pt>
2021-03-22 16:38:00 +0000

    Oi

--- src/stegozoa_hooks/stegozoa_hooks.c
+++ src/stegozoa_hooks/stegozoa_hooks.c
@@ -96,9 +96,10 @@ void readQdct(short *qcoeff, int has_y2_block) {
     for(int i = 0; i < 384 + has_y2_block * 16; i++) {
         if((!has_y2_block || i % 16 != 0 || i > 255) && qcoeff[i] != 1 && qcoeff[i] != 0) {
             theMsg[msgCharDec++] = qcoeff[i];
-            printf("msgCharDec: %c, hex:%x\n", theMsg[msgCharDec-1], theMsg[msgCharDec-1]);
-            if(msgCharDec == 1 && theMsg[0] != '!')
+            if(msgCharDec == 1 && theMsg[0] != '!') {
+                printf("OhNO\n");
                 msgCharDec = 0;
+            }
             if(msgCharDec == sizeof(msg) - 1) {
                 theMsg[msgCharDec] = '\0';
                 printf("Message: %s\n", theMsg);
