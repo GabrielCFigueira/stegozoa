9276636b56020cbb976d26194ff69ca62a92de46
Gabriel Figueira <gabriel.figueira@tecnico.ulisboa.pt>
2021-03-22 15:32:01 +0000

    something is fishy

--- src/stegozoa_hooks/stegozoa_hooks.c
+++ src/stegozoa_hooks/stegozoa_hooks.c
@@ -95,7 +95,7 @@ void readQdct(short *qcoeff, int has_y2_block) {
     for(int i = 0; i < 384 + has_y2_block * 16; i++) {
         if((!has_y2_block || i % 16 != 0 || i > 255) && qcoeff[i] != 1 && qcoeff[i] != 0) {
             theMsg[msgCharDec++] = qcoeff[i];
-
+            printf("msgCharDec: %d\n", msgCharDec);
             if(theMsg[0] != '!')
                 msgCharDec = 0;
             else if(msgCharDec == sizeof(msg)) {
