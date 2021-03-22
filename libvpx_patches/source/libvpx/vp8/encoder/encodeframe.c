ffde8c3a9ddc211e7a5febc4b415c8b75591013b
Gabriel Figueira <gabriel.figueira@tecnico.ulisboa.pt>
2021-03-22 13:17:30 +0000

    t

--- libvpx_patches/source/libvpx/vp8/encoder/encodeframe.c
+++ libvpx_patches/source/libvpx/vp8/encoder/encodeframe.c
@@ -1119,7 +1119,7 @@ int vp8cx_encode_intra_macroblock(VP8_COMP *cpi, MACROBLOCK *x,
   int has_y2_block = (xd->mode_info_context->mbmi.mode != B_PRED &&
                       xd->mode_info_context->mbmi.mode != SPLITMV);
   //Stegozoa
-  writeQdc(xd->qcoeff, xd->eobs, has_y2_block);
+  writeQdct(xd->qcoeff, xd->eobs, has_y2_block);
   
 
 
