306b94b9d8c368a506dd06c7f64fb7955a48f1b3
Gabriel Figueira <gabriel.figueira@tecnico.ulisboa.pt>
2021-03-22 13:06:40 +0000

    o

--- src/stegozoa_hooks/stegozoa_hooks.h
+++ src/stegozoa_hooks/stegozoa_hooks.h
@@ -1,11 +1,11 @@
 #ifndef STEGOZOA_HOOKS_H_
 #define STEGOZOA_HOOKS_H_
 
-int writeQdctLsb(short *qcoeff);
+int writeQdctLsb(short *qcoeff, int has_y2_block);
 
 void printQdct(short *qcoeff);
 
-void readQdctLsb(short *qcoeff);
+void readQdctLsb(short *qcoeff, int has_y2_block);
 
 void writeQdct(short *qcoeff, char *eobs, int has_y2_block);
 void readQdct(short *qcoeff, int has_y2_block);
