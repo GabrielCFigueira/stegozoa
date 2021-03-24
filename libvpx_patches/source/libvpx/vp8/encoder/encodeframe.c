3ac3dc55eebd8b8579bd7da42642418203c19f2a
Gabriel Figueira <gabriel.figueira@tecnico.ulisboa.pt>
2021-03-24 12:37:24 +0000

    seg fault nice

--- libvpx_patches/source/libvpx/vp8/encoder/encodeframe.c
+++ libvpx_patches/source/libvpx/vp8/encoder/encodeframe.c
@@ -898,7 +898,6 @@ void vp8_encode_frame(VP8_COMP *cpi) {
 
         for (int mb_col = 0; mb_col < cm->mb_cols; ++mb_col) {
             
-            printf("Column loop\n");
             vp8_zero(cm->left_context);    
             memcpy(xd->qcoeff, qcoeff, 400 * sizeof(short));
             memcpy(xd->eobs, eobs, 25 * sizeof(char));
@@ -917,6 +916,7 @@ void vp8_encode_frame(VP8_COMP *cpi) {
         }
 
     }
+    printf("After token loop\n");
 
     cpi->tok_count = (unsigned int)(tp - cpi->tok);
 
