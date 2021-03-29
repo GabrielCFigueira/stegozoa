40b178e4b25884c0a7cc59978c485f979f8e37ef
Gabriel Figueira <gabriel.figueira@tecnico.ulisboa.pt>
2021-03-29 19:19:32 +0100

    writer

--- src/stegozoa.py
+++ src/stegozoa.py
@@ -2,19 +2,28 @@ import os
 
 
 
-
-
-
-
-decoderPipe = "/tmp/stegozoa_decoder_pipe"
-encoderPipe = "/tmp/stegozoa_encoder_pipe"
+decoderPipePath = "/tmp/stegozoa_decoder_pipe"
+encoderPipePath = "/tmp/stegozoa_encoder_pipe"
 
 try:
-    os.mkfifo(encoderPipe)
-except OSError as oe: 
+    os.mkfifo(encoderPipePath)
+except Exception as oe: 
     raise ValueError(oe)
 
 try:
-    os.mkfifo(decoderPipe)
-except OSError as oe: 
+    os.mkfifo(decoderPipePath)
+except Exception as oe: 
     raise ValueError(oe)
+
+
+
+
+encoderPipe = open(encoderPipePath)
+message = 'A' * 10000
+encoderPipe.write(message)
+encoderPipe.close()
+
+
+
+os.remove(encoderPipePath)
+os.remove(decoderPipePath)
