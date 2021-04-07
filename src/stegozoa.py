ea2043afbc049251a29fe3fd7b3f899688dda5f8
Gabriel Figueira <gabriel.figueira@tecnico.ulisboa.pt>
2021-04-07 12:17:49 +0100

    sleep

--- src/stegozoa.py
+++ src/stegozoa.py
@@ -135,6 +135,8 @@ if __name__ == "__main__":
     else:
         newId = 1
     connect(newId)
+    while len(getPeers()) < 1:
+        time.sleep(0.5)
     message = "Why are we still here... just to suffer"
     send(bytes(message * 100, 'utf-8'), getPeers()[0])
     print(receive())
