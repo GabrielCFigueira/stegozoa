f9c78bd1e4c05923b9fe11e4448a7dece0355316
Gabriel Figueira <gabriel.figueira@tecnico.ulisboa.pt>
2021-04-10 17:34:46 +0100

    typeid

--- webrtc_patches/modules/video_coding/codecs/vp8/libvpx_vp8_encoder.cc
+++ webrtc_patches/modules/video_coding/codecs/vp8/libvpx_vp8_encoder.cc
@@ -895,6 +895,7 @@ int LibvpxVp8Encoder::Encode(const VideoFrame& frame,
     return WEBRTC_VIDEO_CODEC_UNINITIALIZED;
 
   //Stegozoa
+  std::cout << typeid(encoded_complete_callback_) << std::endl;
   encoded_complete_callback_->getSsrcs();
 
   bool key_frame_requested = false;
