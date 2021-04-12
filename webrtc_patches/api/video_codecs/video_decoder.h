75f9d554df16f84c2d7fde9b67b2edf2f63b8727
Gabriel Figueira <gabriel.figueira@tecnico.ulisboa.pt>
2021-04-12 15:34:03 +0100

    herança

--- webrtc_patches/api/video_codecs/video_decoder.h
+++ webrtc_patches/api/video_codecs/video_decoder.h
@@ -62,7 +62,13 @@ class RTC_EXPORT VideoDecoder {
   //Stegozoa: ssrc
   virtual int32_t Decode(const EncodedImage& input_image,
                          bool missing_frames,
-                         int64_t render_time_ms, uint32_t ssrc = 0) = 0;
+                         int64_t render_time_ms, uint32_t ssrc) {
+	  std::cerr << "Not Implemented!" << std::endl;
+  }
+
+  virtual int32_t Decode(const EncodedImage& input_image,
+                         bool missing_frames,
+                         int64_t render_time_ms) = 0;
 
   virtual int32_t RegisterDecodeCompleteCallback(
       DecodedImageCallback* callback) = 0;
--- webrtc_patches/modules/video_coding/codecs/vp8/libvpx_vp8_decoder.cc
+++ webrtc_patches/modules/video_coding/codecs/vp8/libvpx_vp8_decoder.cc
@@ -182,7 +182,13 @@ int LibvpxVp8Decoder::InitDecode(const VideoCodec* inst, int number_of_cores) {
 
 int LibvpxVp8Decoder::Decode(const EncodedImage& input_image,
                              bool missing_frames,
-                             int64_t /*render_time_ms*/, uint32_t ssrc = 0) {
+                             int64_t /*render_time_ms*/) {
+	return LibvpxVp8Decoder::Decode(input_image, missing_frames, 0, 0);
+}
+
+int LibvpxVp8Decoder::Decode(const EncodedImage& input_image,
+                             bool missing_frames,
+                             int64_t /*render_time_ms*/, uint32_t ssrc) {
   if (!inited_) {
     return WEBRTC_VIDEO_CODEC_UNINITIALIZED;
   }
