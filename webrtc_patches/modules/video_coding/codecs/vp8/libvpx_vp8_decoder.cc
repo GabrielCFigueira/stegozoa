283182dc756e5a41584045cf2c0cf8bb44973393
Gabriel Figueira <gabriel.figueira@tecnico.ulisboa.pt>
2021-04-12 15:20:50 +0100

    pass ssrc

--- webrtc_patches/modules/video_coding/codecs/vp8/libvpx_vp8_decoder.cc
+++ webrtc_patches/modules/video_coding/codecs/vp8/libvpx_vp8_decoder.cc
@@ -182,10 +182,7 @@ int LibvpxVp8Decoder::InitDecode(const VideoCodec* inst, int number_of_cores) {
 
 int LibvpxVp8Decoder::Decode(const EncodedImage& input_image,
                              bool missing_frames,
-                             int64_t /*render_time_ms*/) {
-  //Stegozoa
-  std::raise(SIGSEGV);
-
+                             int64_t /*render_time_ms*/, uint32_t ssrc = 0) {
   if (!inited_) {
     return WEBRTC_VIDEO_CODEC_UNINITIALIZED;
   }
