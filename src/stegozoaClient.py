5c21a2e7d89e8d0c6276a81fdd409a3299742f6c
Gabriel Figueira <gabriel.figueira@tecnico.ulisboa.pt>
2021-04-24 18:41:47 +0100

    client

--- src/stegozoaClient.py
+++ src/stegozoaClient.py
@@ -1,3 +1,35 @@
 import libstegozoa
 
+import os
+import socket
+import threading
 
+socketPath = "/tmp/stegozoa_client_socket"
+
+server = socket.socket(socket.AF_UNIX, socket.SOCK_STREAM)
+
+server.bind(socketPath)
+server.listen(1)
+conn, addr = server.accept()
+
+def send():
+    while True:
+        message = conn.recv(10000)
+        if message:
+            libstegozoa.send(message, 255) #255 is the broadcast address
+
+def receive():
+    while True:
+        message = libstegozoa.receive()
+        conn.send(message)
+
+thread = threading.Thread(target=send, args=())
+thread.start()
+thread = threading.Thread(target=receive, args=())
+thread.start()
+
+
+if __name__ == "__main__":
+    while True:
+        conn.send(bytes("why are we still here... just to suffer?", ascii))
+        print(conn.recv(10000))
