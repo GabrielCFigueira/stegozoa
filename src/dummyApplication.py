0c701c1390c21f28bfe8a8024c6ef51250ac812f
Gabriel Figueira <gabriel.figueira@tecnico.ulisboa.pt>
2021-04-25 17:43:39 +0100

    desempenho

--- src/dummyApplication.py
+++ src/dummyApplication.py
@@ -9,7 +9,13 @@ socketPath = "/tmp/stegozoa_client_socket"
 client = socket.socket(socket.AF_UNIX, socket.SOCK_STREAM)
 client.connect(socketPath)
 
-while True:
+start = time.clock()
+data = 0
+
+for i in range(200):
     client.send(bytes("why are we still here... just to suffer?", 'ascii'))
     message = client.recv(10000)
-    print(message)
+    data += len(message)
+
+end = time.clock()
+print("Throughput: " + str(data / (end - start)))
