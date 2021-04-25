3222e1a6a3d6ba1cc006986c24203b3f6ec7a9aa
Gabriel Figueira <gabriel.figueira@tecnico.ulisboa.pt>
2021-04-25 17:28:05 +0100

    \t

--- src/stegozoaClient.py
+++ src/stegozoaClient.py
@@ -12,8 +12,8 @@ def is_socket_closed(sock):
     try:
     # this will try to read bytes without blocking and also without removing them from buffer (peek only)
         data = sock.recv(16, socket.MSG_DONTWAIT | socket.MSG_PEEK)
-    if len(data) == 0:
-        return True
+        if len(data) == 0:
+            return True
     except BlockingIOError:
         return False  # socket is open and reading from it would block
     except ConnectionResetError:
