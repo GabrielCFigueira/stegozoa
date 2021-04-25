aeb8931044410a3e6a853ca7531721efbf2bc3bc
Gabriel Figueira <gabriel.figueira@tecnico.ulisboa.pt>
2021-04-25 17:18:21 +0100

    no sleep

--- src/dummyApplication.py
+++ src/dummyApplication.py
@@ -9,7 +9,6 @@ socketPath = "/tmp/stegozoa_client_socket"
 client = socket.socket(socket.AF_UNIX, socket.SOCK_STREAM)
 client.connect(socketPath)
 
-time.sleep(15)
 while True:
     client.send(bytes("why are we still here... just to suffer?", 'ascii'))
     message = client.recv(10000)
