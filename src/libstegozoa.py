9738f12fd2705c38a1caab7bd419b8e49a47da53
Gabriel Figueira <gabriel.figueira@tecnico.ulisboa.pt>
2021-05-03 18:14:49 +0100

    who

--- src/libstegozoa.py
+++ src/libstegozoa.py
@@ -181,6 +181,6 @@ if __name__ == "__main__":
     while len(getPeers()) < 1:
         time.sleep(0.5)
     message = "Why are we still here... just to suffer"
-    send(bytes(message * 100, 'utf-8'), getPeers()[0])
+    send(bytes(message, 'utf-8'), getPeers()[0])
     print(receive())
 
