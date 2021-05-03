c819f2c352ff0e38d8ded829244e63d954aedd55
Gabriel Figueira <gabriel.figueira@tecnico.ulisboa.pt>
2021-05-03 20:33:58 +0100

    what about

--- src/stegozoa_hooks/stegozoa_hooks.c
+++ src/stegozoa_hooks/stegozoa_hooks.c
@@ -346,8 +346,8 @@ int readQdctLsb(short *qcoeff, int has_y2_block, uint32_t ssrc) {
             
             if(msg->bit == 32) { //future idea: completely discard the newConstant, by making sure it is always fully contained in a frame TODO
                 uint32_t newConstant = obtainConstant(msg->buffer);
+                printf("Ssrc: %lu Constant: %x\n", (unsigned long) ssrc, newConstant);
                 if(newConstant != constant) {
-                    printf("Ssrc: %lu Constant discarded: %x\n", (unsigned long) ssrc, newConstant);
                     newConstant = newConstant >> 1;
                     insertConstant(newConstant, msg->buffer);
                     msg->bit--;
