425ff523b738a2cbbdf432b0b3235c602006e8f2
Gabriel Figueira <gabriel.figueira@tecnico.ulisboa.pt>
2021-05-23 12:24:58 +0100

    sighandler

--- src/stegozoaClient.py
+++ src/stegozoaClient.py
@@ -28,6 +28,8 @@ if len(sys.argv) > 1:
 else:
     myId = 1
 
+signal.signal(signal.SIGINT,libstegozoa.sigInt_handler)
+
 server = socket.socket(socket.AF_UNIX, socket.SOCK_STREAM)
 
 mutex = threading.Lock()
