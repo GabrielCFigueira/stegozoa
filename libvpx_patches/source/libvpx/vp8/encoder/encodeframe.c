204f2145e2ab2873d28763844ab589f62ff42aa6
Gabriel Figueira <gabriel.figueira@tecnico.ulisboa.pt>
2021-06-12 13:24:27 +0100

    lets see

--- libvpx_patches/source/libvpx/vp8/encoder/encodeframe.c
+++ libvpx_patches/source/libvpx/vp8/encoder/encodeframe.c
@@ -951,7 +951,7 @@ void vp8_encode_frame(VP8_COMP *cpi) {
     }
 
     end = clock();
-    printf("Time spent generating tokens %d: %lf\n", cm->current_video_frame, ((double) end - start) / CLOCKS_PER_SEC);
+    printf("Time spent generating tokens %d: %lf, capacity:%d\n", cm->current_video_frame, ((double) end - start) / CLOCKS_PER_SEC, cpi->bits);
     //Stegozoa: embedding capacity
     //fprintf(stdout, "Ssrc: %lu. Current Frame: %d. Embbed data: %d. Capacity: %d\n", (unsigned long) cpi->ssrc, cpi->common.current_video_frame, embbedData, cpi->bits);
     //fflush(stdout);
