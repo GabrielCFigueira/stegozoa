f498e10154fd5db874835e854d9a70ad7e8c51cc
Gabriel Figueira <gabriel.figueira@tecnico.ulisboa.pt>
2021-06-23 12:03:17 +0100

    bruh

--- libvpx_patches/source/libvpx/vp8/encoder/encodeframe.c
+++ libvpx_patches/source/libvpx/vp8/encoder/encodeframe.c
@@ -1201,8 +1201,8 @@ int vp8cx_encode_intra_macroblock(VP8_COMP *cpi, MACROBLOCK *x,
   int has_y2_block = (xd->mode_info_context->mbmi.mode != B_PRED &&
           xd->mode_info_context->mbmi.mode != SPLITMV);
   
-  for(int i = 0; i < 384 /*384 + has_y2_block * 16*/; i++)
-    if(xd->qcoeff[i] != 1 && xd->qcoeff[i] != 0 && (i % 16 != 0 || i > 255)) { // (!has_y2_block || i % 16 != 0 || i > 255)) {
+  for(int i = 0; i < 384; i++)
+    if(xd->qcoeff[i] != 1 && xd->qcoeff[i] != 0 && i % 16 != 0) {
       cpi->positions[mb_row][cpi->row_bits[mb_row]] = i + (mb_row * cpi->common.mb_cols + mb_col) * 400;
       cpi->cover[mb_row][cpi->row_bits[mb_row]] = xd->qcoeff[i] & 0x1;
       cpi->row_bits[mb_row]++;
@@ -1384,7 +1384,7 @@ int vp8cx_encode_inter_macroblock(VP8_COMP *cpi, MACROBLOCK *x, TOKENEXTRA **t,
           xd->mode_info_context->mbmi.mode != SPLITMV);
   
   for(int i = 0; i < 384; i++)
-    if(xd->qcoeff[i] != 1 && xd->qcoeff[i] != 0 && (i % 16 != 0 || i > 255)) { // (!has_y2_block || i % 16 != 0 || i > 255)) {
+    if(xd->qcoeff[i] != 1 && xd->qcoeff[i] != 0 && i % 16 != 0) {
       cpi->positions[mb_row][cpi->row_bits[mb_row]] = i + (mb_row * cpi->common.mb_cols + mb_col) * 400;
       cpi->cover[mb_row][cpi->row_bits[mb_row]] = xd->qcoeff[i] & 0x1;
       cpi->row_bits[mb_row]++;
