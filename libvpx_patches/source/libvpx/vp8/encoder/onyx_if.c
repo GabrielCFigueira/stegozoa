1f2c657739c1bf0521b526ca539e2fd1df243904
Gabriel Figueira <gabriel.figueira@tecnico.ulisboa.pt>
2021-06-29 12:49:36 +0100

    moved the code

--- libvpx_patches/source/libvpx/vp8/encoder/onyx_if.c
+++ libvpx_patches/source/libvpx/vp8/encoder/onyx_if.c
@@ -5179,6 +5179,63 @@ int vp8_get_compressed_data(VP8_COMP *cpi, unsigned int *frame_flags,
     generate_psnr_packet(cpi);
   }
 
+  //Stegozoa: psnr and ssim
+  if (cm->show_frame) {
+    cpi->common.show_frame_mi = cpi->common.mi;
+    cpi->count++;
+
+      
+    uint64_t ye, ue, ve;
+    double frame_psnr;
+    YV12_BUFFER_CONFIG *orig = cpi->Source;
+    YV12_BUFFER_CONFIG *recon = cpi->common.frame_to_show;
+    unsigned int y_width = cpi->common.Width;
+    unsigned int y_height = cpi->common.Height;
+    unsigned int uv_width = (y_width + 1) / 2;
+    unsigned int uv_height = (y_height + 1) / 2;
+    int y_samples = y_height * y_width;
+    int uv_samples = uv_height * uv_width;
+    int t_samples = y_samples + 2 * uv_samples;
+    double sq_error;
+
+    ye = calc_plane_error(orig->y_buffer, orig->y_stride, recon->y_buffer,
+                              recon->y_stride, y_width, y_height);
+
+    ue = calc_plane_error(orig->u_buffer, orig->uv_stride, recon->u_buffer,
+                              recon->uv_stride, uv_width, uv_height);
+     
+    ve = calc_plane_error(orig->v_buffer, orig->uv_stride, recon->v_buffer,
+                              recon->uv_stride, uv_width, uv_height);
+   
+    sq_error = (double)(ye + ue + ve);
+ 
+    frame_psnr = vpx_sse_to_psnr(t_samples, 255.0, sq_error);
+
+    printf("PSNR (before deblocking): %f\n", frame_psnr);
+    YV12_BUFFER_CONFIG *pp = &cm->post_proc_buffer;
+    double sq_error2;
+    double frame_psnr2, frame_ssim2 = 0;
+    double weight = 0;
+
+    vp8_deblock(cm, cm->frame_to_show, &cm->post_proc_buffer,
+                  cm->filter_level * 10 / 6);
+    vpx_clear_system_state();
+
+    ye = calc_plane_error(orig->y_buffer, orig->y_stride, pp->y_buffer,
+                            pp->y_stride, y_width, y_height);
+    ue = calc_plane_error(orig->u_buffer, orig->uv_stride, pp->u_buffer,
+                            pp->uv_stride, uv_width, uv_height);
+
+    ve = calc_plane_error(orig->v_buffer, orig->uv_stride, pp->v_buffer,
+                            pp->uv_stride, uv_width, uv_height);
+
+    sq_error2 = (double)(ye + ue + ve);
+
+    frame_psnr2 = vpx_sse_to_psnr(t_samples, 255.0, sq_error2);
+    frame_ssim2 = vpx_calc_ssim(cpi->Source, &cm->post_proc_buffer, &weight);
+    printf("PSNR (after deblocking): %f, SSIM: %f", frame_psnr2, frame_ssim2);
+  }
+
 #if CONFIG_INTERNAL_STATS
 
   if (cpi->pass != 1) {
@@ -5222,8 +5279,6 @@ int vp8_get_compressed_data(VP8_COMP *cpi, unsigned int *frame_flags,
         cpi->total += frame_psnr;
 #if CONFIG_POSTPROC
         {
-          //Stegozoa
-          printf("Passei aqui!!\n");
 
           YV12_BUFFER_CONFIG *pp = &cm->post_proc_buffer;
           double sq_error2;
