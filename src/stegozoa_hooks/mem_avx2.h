f3871e933a9720d20d3935068679ddecc76bf1ea
Gabriel Figueira <gabriel.figueira@tecnico.ulisboa.pt>
2021-08-12 20:04:43 +0100

    letss

--- src/stegozoa_hooks/mem_avx2.h
+++ src/stegozoa_hooks/mem_avx2.h
@@ -4,8 +4,8 @@
 
 
 
-void copy_256(void *d, const void *s)
-void coeff_copy_400(void *d, const void *s)
-void eobs_copy_32(void *d, const void *s)
+void copy_256(void *d, const void *s);
+void coeff_copy_400(void *d, const void *s);
+void eobs_copy_32(void *d, const void *s);
 
 #endif
