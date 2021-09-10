de4605272f64111e950f5f58cf6def6399a77163
Gabriel Figueira <gabriel.figueira@tecnico.ulisboa.pt>
2021-09-10 10:27:05 +0100

    revert to w=4

--- src/stegozoa_hooks/stegozoa_hooks.c
+++ src/stegozoa_hooks/stegozoa_hooks.c
@@ -442,8 +442,8 @@ static void discardMessage(context_t *ctx) {
 static int obtainMessage(context_t *ctx, int size) {
     
     message_t *msg = ctx->msg;
-    //unsigned char *message = ctx->stcData->message;
-    unsigned char *message = ctx->stcData->steganogram;
+    unsigned char *message = ctx->stcData->message;
+    //unsigned char *message = ctx->stcData->steganogram;
 
     int toSend = 0;
 
@@ -627,8 +627,8 @@ int flushEncoder(uint32_t ssrc, int simulcast, int size) {
     context_t *ctx = getEncoderContext(ssrc);
     stc_data_t *data = ctx->stcData;
 
-    //int msgSize = size / WIDTH;
-    int msgSize = size;
+    int msgSize = size / WIDTH;
+    //int msgSize = size;
 
     int toSend = obtainMessage(ctx, msgSize);
 
@@ -637,7 +637,7 @@ int flushEncoder(uint32_t ssrc, int simulcast, int size) {
         return 0; //should abort
     }
 
-    //stc(size, data);
+    stc(size, data);
 
     return toSend;
 
@@ -705,10 +705,10 @@ int readQdctLsb(unsigned char **cover, int *row_bits, int n_rows, unsigned char*
 
 void flushDecoder(unsigned char *steganogram, unsigned char *message, uint32_t ssrc, uint64_t rtpSession, int size) {
 
-    //int msgSize = size / WIDTH;
-    //reverseStc(steganogram, message, size);
-    int msgSize = size;
-    message = steganogram;
+    int msgSize = size / WIDTH;
+    reverseStc(steganogram, message, size);
+    //int msgSize = size;
+    //message = steganogram;
 
     message_t *msg = getDecoderContext(ssrc, rtpSession)->msg;
 
