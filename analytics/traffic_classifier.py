ca7daf684792bdec04f9c4efee94817446160d26
Gabriel Figueira <gabriel.figueira@tecnico.ulisboa.pt>
2021-10-06 22:32:27 +0100

    how about this?

--- analytics/traffic_classifier.py
+++ analytics/traffic_classifier.py
@@ -179,7 +179,8 @@ def runClassificationKFold_CV(data_folder, mode, cfg, classifier, comparison, lo
 
 
     #Figure properties
-    fig = plt.figure(figsize=[7.6, 6.4])
+    #fig = plt.figure(figsize=[7.6, 6.4])
+    fig = plt.figure()
     ax1 = fig.add_subplot(111)
 
     std_auc = np.std(aucs)
@@ -206,8 +207,8 @@ def runClassificationKFold_CV(data_folder, mode, cfg, classifier, comparison, lo
     ax1.spines['right'].set_visible(False)
     ax1.spines['top'].set_visible(False)
 
-    plt.xlabel('False Positive Rate', fontsize='26')
-    plt.ylabel('True Positive Rate', fontsize='24')
+    plt.xlabel('False Positive Rate', fontsize=26)
+    plt.ylabel('True Positive Rate', fontsize=24)
     plt.legend(loc='lower right', frameon=False, handlelength=1.0, fontsize=14)
     #plt.tight_layout()
 
