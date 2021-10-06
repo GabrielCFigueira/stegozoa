0ea39bbf7f38164a121d8121dccf35fd32036221
Gabriel Figueira <gabriel.figueira@tecnico.ulisboa.pt>
2021-10-06 15:29:30 +0100

    handle

--- analytics/traffic_classifier.py
+++ analytics/traffic_classifier.py
@@ -208,7 +208,7 @@ def runClassificationKFold_CV(data_folder, mode, cfg, classifier, comparison, lo
 
     plt.xlabel('False Positive Rate', fontsize='26')
     plt.ylabel('True Positive Rate', fontsize='24')
-    plt.legend(loc='lower right', frameon=False, handlength=1.0, fontsize=14)
+    plt.legend(loc='lower right', frameon=False, handlelength=1.0, fontsize=14)
     plt.tight_layout()
 
     plt.setp(ax1.get_xticklabels(), fontsize=20)
